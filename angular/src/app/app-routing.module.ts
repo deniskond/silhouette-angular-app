6443bde56fefb53235a9669da07c785d477f3082
Denis Kondarattsev <deniskond@yandex.ru>
2020-03-29 11:27:24 +0300

    feat(routing): added routing and components markup

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FeaturesPageComponent } from './pages/features-page/features-page.component';
+import { TodoPageComponent } from './pages/todo-page/todo-page.component';
+import { ChangelogPageComponent } from './pages/changelog-page/changelog-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
-const routes: Routes = [{ path: '', component: FeaturesPageComponent }];
+const routes: Routes = [
+    { path: '', component: FeaturesPageComponent },
+    { path: 'todo', component: TodoPageComponent },
+    { path: 'changelog', component: ChangelogPageComponent },
+    { path: 'about', component: AboutPageComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
